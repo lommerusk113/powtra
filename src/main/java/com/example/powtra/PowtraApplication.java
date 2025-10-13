01d92ab996a0da7bb29e042c4505f705106f8ee1
Joakim <mrjoakim112@gmail.com>
2025-10-13 21:40:32 +0200

    ## - Initial commit

---
+++ src/main/java/com/example/powtra/PowtraApplication.java
@@ -0,0 +1,13 @@
+package com.example.powtra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowtraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowtraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/powtra/PowtraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.powtra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowtraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
